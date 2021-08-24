da9b3ea2f7ad1b5f467ae0cffef9e2d318fc3c16
Sin Kim <kimsin98@gmail.com>
2021-08-24 22:00:22 +0900

    ComBat script

---
+++ code/scanner_combat.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from pathlib import Path
+import pandas as pd
+
+from utils import abcd
+from utils.combat import combat
+
+
+covariates = abcd.load_covariates(covars=['interview_age', 'sex', 'hisp'],
+                                  simple_race=True)
+
+fcon, fc_extra = abcd.load_mri_data('fcon', dropna=True)
+scon, sc_extra = abcd.load_mri_data('scon', dropna=True)
+
+def save_scan_combat(filename, con, extra, scan_confounds):
+    batch = extra[scan_confounds].apply(tuple, axis=1)
+    covars = extra[['meanmotion']].join(covariates)
+    adjusted = combat(con, batch, covars, n_procs=4)
+    adjusted.to_csv(abcd.OUTPUT / filename)
+
+save_scan_combat('fcon-device.csv', fcon, fc_extra,
+                 ['mri_info_deviceserialnumber'])
+save_scan_combat('fcon-device-software.csv', fcon, fc_extra,
+                 ['mri_info_deviceserialnumber', 'mri_info_softwareversion'])
+save_scan_combat('scon-device.csv', scon, sc_extra,
+                 ['mri_info_deviceserialnumber'])
+save_scan_combat('scon-device-software.csv', scon, sc_extra,
+                 ['mri_info_deviceserialnumber', 'mri_info_softwareversion'])
